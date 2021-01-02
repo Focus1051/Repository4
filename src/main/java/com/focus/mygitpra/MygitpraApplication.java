06f7a9b04078fd5c65a3f48560779ddbfcbb42ee
ljm <2246514127@qq.com>
2021-01-02 12:30:05 +0800

    First Git Practice

---
+++ src/main/java/com/focus/mygitpra/MygitpraApplication.java
@@ -0,0 +1,13 @@
+package com.focus.mygitpra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygitpraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MygitpraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/focus/mygitpra/MygitpraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.focus.mygitpra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygitpraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
